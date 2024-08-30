9a6d418b2c9cdb82bd6e3be101aea78af3546c58
Pranav <srinivaspranav1031@gmail.com>
2024-08-30 23:31:14 +0530

    "nav change"

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     return(
       <header className="header">
         <nav className="nav container">
-            <a href="index.html" className="nav__log"> Srinivas Pranav</a>
+            <a href="index.html" className="nav__logo"> Srinivas Pranav</a>
 
             <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
                 <ul className="nav__list grid">
